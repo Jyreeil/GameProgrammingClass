475579f50bf0ae8d1c148ee088171ce3917a368f
Jyreeil <triplem577@gmail.com>
2016-12-06 17:08:32 -0600

    One script from survival shooter
    Here is one of the scripts from survival shooter, there are many more, but I can't figure out how to upload multiple separate scripts to one repository

---
+++ CollectPowerUp.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollectPowerUp : MonoBehaviour {
+
+    public static bool isInvulnerable;
+    public static bool fasterShooting;
+
+    GameObject player;
+    PlayerHealth playerHealth;
+
+    void Start()
+    {
+        isInvulnerable = false;
+        fasterShooting = false;
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerHealth = player.GetComponent<PlayerHealth>();
+    }
+	void OnTriggerEnter(Collider other)
+    {
+        if (gameObject.tag == "Coin" && other.tag == "Player")
+        {
+            Destroy(gameObject);
+            ScoreManager.coin += 1;
+        }
+        if (gameObject.tag == "Health" && other.tag == "Player")
+        {
+            Destroy(gameObject);
+            playerHealth.TakeDamage(-10);
+        }
+        if (gameObject.tag == "FasterShooting" && other.tag == "Player")
+        {
+            Destroy(gameObject);
+            fasterShooting = true;
+            StartCoroutine(RapidFireWait(gameObject));
+        }
+        if (gameObject.tag == "Invulnerability" && other.tag == "Player")
+        {
+            Destroy(gameObject);
+            isInvulnerable = true;
+            StartCoroutine(InvulnWait(gameObject));
+        }
+
+    }
+    IEnumerator RapidFireWait(GameObject gameObject)
+    {
+        yield return new WaitForSeconds(20);
+        fasterShooting = false;
+    }
+    IEnumerator InvulnWait(GameObject gameObject)
+    {
+        yield return new WaitForSeconds(20);
+        isInvulnerable = false;
+    }
+}
